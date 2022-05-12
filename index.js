7092081f75b2ad6e8b09fbd2bbea2a042f0d4ea9
Горохов Павел Олегович (4066367) <pgorohov@vtb.ru>
2022-05-13 00:19:55 +0300

    init

---
+++ index.js
@@ -0,0 +1,15 @@
+const telegramApi = require('node-telegram-bot-api')
+
+const token = '5215511034:AAE5pPVxjktHrXQEOPjWsFQD1n4rw5Rj3bQ'
+
+const bot = new telegramApi(token, {polling: true})
+
+bot.on('message', mes=>{
+
+    // console.log(mes)
+
+    const text = mes.text;
+    const chatId = mes.chat.id
+
+    bot.sendMessage(chatId, `Hello, ${text}`)
+})
