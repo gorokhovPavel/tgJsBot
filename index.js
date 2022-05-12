2c2d090042b2917cf7237cf519231d733c71bcab
Горохов Павел Олегович (4066367) <pgorohov@vtb.ru>
2022-05-13 01:29:04 +0300

    init3

--- index.js
+++ index.js
@@ -4,10 +4,53 @@ const token = '5215511034:AAE5pPVxjktHrXQEOPjWsFQD1n4rw5Rj3bQ'
 
 const bot = new telegramApi(token, {polling: true})
 
-bot.on('message', mes=>{
+const chats = {}
 
-    const text = mes.text;
-    const chatId = mes.chat.id
+const gameOptions = {
+    reply_markup: JSON.stringify({
+        inline_keyboard: [
+            [{text: '1', callback_data: '1'}],
+            [{text: '2', callback_data: '2'}],
+            [{text: '3', callback_data: '3'}],
+            [{text: '4', callback_data: '4'}]
+        ]
+    })
+}
 
-    bot.sendMessage(chatId, `Hello, ${text}`)
-})
+const start = () =>{
+    bot.setMyCommands([
+        {command: 'start', description: 'wer'},
+        {command: 'info', description: 'wer'},
+        {command: 'game', description: 'wer'},
+    ])
+
+    bot.on('message', async mes=>{
+
+        const text = mes.text;
+        const chatId = mes.chat.id
+
+        if (text === '/start'){
+            return bot.sendMessage(chatId, `Hello, ${text}`)
+        }
+
+        if(text === '/info'){
+            return bot.sendMessage(chatId, `This is tgBot`)
+        }
+
+        if(text === '/game'){
+            await bot.sendMessage(chatId, 'Now let s try to know my number!')
+            const rnd = Math.floor(Math.random()* 10)
+            chats[chatId] = rnd
+
+            return bot.sendMessage(chatId, 'Go!', gameOptions)
+        }
+
+        return bot.sendMessage(chatId, 'don t understand')
+    })
+
+    bot.on('callback_query', msg=>{
+        console.log(msg)
+    })
+}
+
+start()
