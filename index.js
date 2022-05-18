2d3625a9bf78f4f8a2b4cc185806094d54ed9e3f
gorokhovPavel <36862528+gorokhovPavel@users.noreply.github.com>
2022-05-18 23:10:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const telegramApi = require('node-telegram-bot-api')
 
 const token = '5215511034:AAE5pPVxjktHrXQEOPjWsFQD1n4rw5Rj3bQ'
-
 const bot = new telegramApi(token, {polling: true})
 
 const chats = {}
