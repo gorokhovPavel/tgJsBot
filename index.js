2ad68f9379b7c545d27c67f6568bcc91f353c3ae
gorokhovPavel <36862528+gorokhovPavel@users.noreply.github.com>
2022-07-07 11:33:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const gameOptions = {
     reply_markup: JSON.stringify({
         inline_keyboard: [
             [{text: '1', callback_data: '1'}],
+            
             [{text: '2', callback_data: '2'}],
             
             [{text: '3', callback_data: '3'}],
