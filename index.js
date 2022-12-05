9b65b7395430257c4623ff597e9679e64f979271
gorokhovPavel <36862528+gorokhovPavel@users.noreply.github.com>
2022-12-05 10:36:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const bot = new telegramApi(token, {polling: true})
 //https://habr.com/ru/post/666278/
 const chats = {}
 
+
 const gameOptions = {
     reply_markup: JSON.stringify({
         inline_keyboard: [
